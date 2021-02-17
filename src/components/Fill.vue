e0ccf5678486a87da6c7a1a10882cb4c97c18700
martacamacho <martacamachovargas@gmail.com>
2021-02-17 21:12:24 +0100

    exercici 2 fet

---
+++ src/components/Fill.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Fill">
+   <p>Papá mio</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+          menssage: 'Soc el Fill'
+      }
+  }
+}
+</script>
+
+<style scoped>
+</style>
